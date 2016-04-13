8c56498dd7fcdb35ad59acd1bd54a3e179336113
ikeda <ikeda@IKEDA-MBA.local>
2016-04-13 13:21:45 +0900

    Modify __future__ import

--- scripts/partial_dos.py
+++ scripts/partial_dos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 __author__ = "Yuji Ikeda"
 
