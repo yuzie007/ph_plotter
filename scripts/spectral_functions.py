6329f95c3c7c222c883269a2379380ffdb7703d6
ikeda <ikeda@IKEDA-MBA.local>
2016-04-13 23:11:53 +0900

    Modify scripts to call CommonArgumentsAdder

--- scripts/spectral_functions.py
+++ scripts/spectral_functions.py
@@ -5,6 +5,8 @@ from __future__ import (absolute_import, division,
 
 __author__ = "Yuji Ikeda"
 
+from ph_plotter.common_arguments_adder import CommonArgumentsAdder
+
 
 def run(variables):
     from ph_plotter.spectral_functions_plotter import SpectralFunctionsPlotter
@@ -14,48 +16,7 @@ def run(variables):
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--f_max",
-                        default=10.0,
-                        type=float,
-                        help="Maximum plotted frequency (THz).")
-    parser.add_argument("--f_min",
-                        default=-2.5,
-                        type=float,
-                        help="Minimum plotted frequency (THz).")
-    parser.add_argument("--d_freq",
-                        default=2.5,
-                        type=float,
-                        help="Pitch for frequency (THz).")
-    parser.add_argument("--linecolor",
-                        default="#ff0000",
-                        type=str,
-                        help="Linecolor.")
-    parser.add_argument("-a", "--alpha",
-                        default=0.2,
-                        type=float,
-                        help="Alpha for the color of low-frequency weights.")
-    parser.add_argument("--sf_max",
-                        type=float,
-                        help="Maximum of spectral functions.")
-    parser.add_argument("--sf_min",
-                        type=float,
-                        help="Minimum of spectral functions.")
-    parser.add_argument("--d_sf",
-                        type=float,
-                        help="Ticks of spectral functions.")
-    parser.add_argument("-t", "--figure_type",
-                        default="pdf",
-                        type=str,
-                        help="Filetype of figures.")
-    parser.add_argument("--figsize",
-                        nargs=2,
-                        default=(5.0, 3.5),
-                        type=float,
-                        help="Filesize of figures.")
-    parser.add_argument("--poscar",
-                        default="POSCAR",
-                        type=str,
-                        help="Filename of POSCAR.")
+    CommonArgumentsAdder().add_common_arguments(parser)
     parser.add_argument("--data_file",
                         default="band.hdf5",
                         type=str,
--- scripts/total_dos.py
+++ scripts/total_dos.py
@@ -5,6 +5,8 @@ from __future__ import (absolute_import, division,
 
 __author__ = "Yuji Ikeda"
 
+from ph_plotter.common_arguments_adder import CommonArgumentsAdder
+
 
 def run(variables):
     from ph_plotter.total_dos_plotter import TotalDOSPlotter
@@ -14,35 +16,7 @@ def run(variables):
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--f_max",
-                        default=10.0,
-                        type=float,
-                        help="Maximum plotted frequency (THz).")
-    parser.add_argument("--f_min",
-                        default=-2.5,
-                        type=float,
-                        help="Minimum plotted frequency (THz).")
-    parser.add_argument("--d_freq",
-                        default=2.5,
-                        type=float,
-                        help="Pitch for frequency (THz).")
-    parser.add_argument("--linecolor",
-                        default="#ff0000",
-                        type=str,
-                        help="Linecolor.")
-    parser.add_argument("-a", "--alpha",
-                        default=0.2,
-                        type=float,
-                        help="Alpha for the color of low-frequency weights.")
-    parser.add_argument("-t", "--figure_type",
-                        default="pdf",
-                        type=str,
-                        help="Filetype of figures.")
-    parser.add_argument("--figsize",
-                        nargs=2,
-                        default=(5.0, 3.5),
-                        type=float,
-                        help="Filesize of figures.")
+    CommonArgumentsAdder().add_common_arguments(parser)
     parser.add_argument("--data_file",
                         default="total_dos.dat",
                         type=str,
