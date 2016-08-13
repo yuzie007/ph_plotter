881d7ed1e2ad8be504c02d29ccaaac4e12ea0605
ikeda <ikeda@IKEDA-MBA.local>
2016-08-13 14:13:23 +0900

    Add the script to plot band structures with bandwidths

---
+++ scripts/band_width.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+__author__ = "Yuji Ikeda"
+
+from ph_plotter.common_arguments_adder import CommonArgumentsAdder
+
+
+def run(variables):
+    from ph_plotter.band_error_plotter import BandWidthPlotter
+    BandWidthPlotter(variables).run()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    CommonArgumentsAdder().add_common_arguments(parser)
+    parser.add_argument("--data_file",
+                        default="sf_fitted.hdf5",
+                        type=str,
+                        help="Filename of data.")
+    args = parser.parse_args()
+
+    print(vars(args))
+    run(vars(args))
+
+
+if __name__ == "__main__":
+    main()
