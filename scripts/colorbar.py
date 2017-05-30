3f2e4d92b6975495aa742651cde1b9f68a978355
ikeda <ikeda@IKEDA-MBA.local>
2017-05-30 18:51:44 +0200

    Modify to give crange and cpitch

--- scripts/colorbar.py
+++ scripts/colorbar.py
@@ -11,27 +11,17 @@ __author__ = "Yuji Ikeda"
 
 
 class Colorbar(object):
-    def __init__(self, colors_p, colors_n, num_p, num_n):
-        self._colors_p = colors_p
-        self._colors_n = colors_n
-        self._num_p = num_p
-        self._num_n = num_n
-        self._cmap = None
+    def create_colormap(self, colors_p, colors_n, crange, cpitch):
+        self._create_bounds(crange, cpitch)
 
-    def create_colormap(self):
-        colormap_creator = ColormapCreator(
-            colors_p=self._colors_p,
-            colors_n=self._colors_n,
-        )
-        self._create_bounds()
+        colormap_creator = ColormapCreator(colors_p=colors_p, colors_n=colors_n)
         cmap = colormap_creator.create_colormap(ticks=self._bounds)
 
         self._cmap = cmap
 
-    def _create_bounds(self):
-        bounds = np.array(
-            range(-self._num_n + 1, 0) + range(self._num_p)
-        )
+    def _create_bounds(self, crange, cpitch):
+        n = int(round((crange[1] - crange[0]) / cpitch)) + 1
+        bounds = np.linspace(crange[0], crange[1], n)
         self._bounds = bounds
 
     def plot(self, figsize, figure_type, save):
@@ -64,14 +54,15 @@ def main():
                         default="w",
                         type=str,
                         help="Colors in the negative region.")
-    parser.add_argument("--np", dest="num_p",
-                        default=1,
-                        type=int,
-                        help="Number of colors in the positive region.")
-    parser.add_argument("--nn", dest="num_n",
-                        default=1,
-                        type=int,
-                        help="Number of colors in the negative region.")
+    parser.add_argument("--range",
+                        nargs=2,
+                        default=[-1.0, 1.0],
+                        type=float,
+                        help="Plotting range")
+    parser.add_argument("--pitch",
+                        default=1.0,
+                        type=float,
+                        help="Plotting pitch")
     parser.add_argument("-a", "--alpha",
                         default=0.2,
                         type=float,
@@ -90,8 +81,8 @@ def main():
                         help="Save the colorbar figure")
     args = parser.parse_args()
 
-    colorbar = Colorbar(args.colors_p, args.colors_n, args.num_p, args.num_n)
-    colorbar.create_colormap()
+    colorbar = Colorbar()
+    colorbar.create_colormap(args.colors_p, args.colors_n, args.range, args.pitch)
     colorbar.plot(args.figsize, args.figure_type, args.save)
 
 
